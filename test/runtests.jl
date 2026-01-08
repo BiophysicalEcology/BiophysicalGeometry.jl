aee22902052fc6bc372d9d075a4b7509b2c69b0f
Rafael Schouten <rafaelschouten@gmail.com>
2026-01-08 22:26:17 +1100

    remove FluidProperties and other unused dependencies (#1)
    * add FluidProperties source
    
    * no Manifest file
    
    * add compat bounds
    
    * fix Aqua
    
    * dependency cleanup
    
    * there are not docs

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,14 @@
 using BiophysicalGeometry
-using SafeTestsets
 using Aqua
+using SafeTestsets
+using Test
 
-Aqua.test_unbound_args(BiophysicalGeometry)
-Aqua.test_stale_deps(BiophysicalGeometry)
-Aqua.test_undefined_exports(BiophysicalGeometry)
-Aqua.test_project_extras(BiophysicalGeometry)
-Aqua.test_deps_compat(BiophysicalGeometry)
-Aqua.test_project_toml_formatting(BiophysicalGeometry)
+@testset "Quality assurance" begin
+    Aqua.test_unbound_args(BiophysicalGeometry)
+    Aqua.test_stale_deps(BiophysicalGeometry)
+    Aqua.test_undefined_exports(BiophysicalGeometry)
+    Aqua.test_project_extras(BiophysicalGeometry)
+    Aqua.test_deps_compat(BiophysicalGeometry)
+end
 
 @safetestset "geometry" begin include("geometry.jl") end
